c30ccc4b8a8f13d5c7e22cf445a4da78059716b5
KReyes8 <karenreyesdgo@hotmail.com>
2025-02-05 19:37:04 -0600

    Add files via upload

---
+++ javascript3.js
@@ -0,0 +1,27 @@
+/*
+Karen Reyes Ramirez
+Universidad Autónoma de Durango
+Examen*/
+let currentIndex = 0; // Menciona la imagen actual //
+
+function showImage(index) {
+    const images = document.querySelectorAll('.carousel-images img');
+    const totalImages = images.length;
+    if (index >= totalImages) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = totalImages - 1;
+    } else {
+        currentIndex = index;
+    }
+    const offset = -currentIndex * 100;
+    document.querySelector('.carousel-images').style.transform = `translateX(${offset}%)`;
+}
+
+function nextImage() { //Para pasar a la siguiente o anterior imagen con las flechas //
+    showImage(currentIndex + 1);
+}
+
+function prevImage() {
+        showImage(currentIndex - 1);
+    }
